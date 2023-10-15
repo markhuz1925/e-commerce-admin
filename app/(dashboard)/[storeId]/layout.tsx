269e16713c28b01088628cf057165a80d92d8af2
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-10-16 01:37:00 +0800

    Create Dashboard page and layout.

---
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -0,0 +1,32 @@
+import prisma from "@/prisma/client";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { ReactNode } from "react";
+
+export default async function DashboardLayout({
+  children,
+  params,
+}: {
+  children: ReactNode;
+  params: { storeId: string };
+}) {
+  const { userId } = auth();
+
+  if (!userId) redirect("/sign-in");
+
+  const store = await prisma.store.findFirst({
+    where: {
+      id: params.storeId,
+      userId,
+    },
+  });
+
+  if (!store) redirect("/");
+
+  return (
+    <div>
+      <div>Navbar goes here!</div>
+      {children}
+    </div>
+  );
+}
