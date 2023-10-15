d99d5b00ef140e427c0f0a3b6bbd3315a83e2173
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-10-16 01:37:44 +0800

    Add root layout and root page.

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,24 @@
+import prisma from "@/prisma/client";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { ReactNode } from "react";
+
+export default async function SetupLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const { userId } = auth();
+
+  if (!userId) redirect("/sign-in");
+
+  const store = await prisma.store.findFirst({
+    where: {
+      userId,
+    },
+  });
+
+  if (store) redirect(`/${store.id}`);
+
+  return <>{children}</>;
+}
