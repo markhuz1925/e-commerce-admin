43ffe502d8678052bc54b3e804e36b0eec758125
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-10-18 22:34:34 +0800

    Build Settings Page.

---
+++ app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -0,0 +1,28 @@
+import prisma from "@/prisma/client";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+export default async function SettingsPage({
+  params,
+}: {
+  params: { storeId: string };
+}) {
+  const { userId } = auth();
+
+  if (!userId) redirect("/sign-in");
+
+  const store = await prisma.store.findFirst({
+    where: {
+      id: params.storeId,
+      userId,
+    },
+  });
+
+  if (!store) redirect("/");
+
+  return (
+    <div className="flex flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">Settings Page</div>
+    </div>
+  );
+}
